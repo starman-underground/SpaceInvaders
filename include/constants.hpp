0c0f947b3284388657716cb381e046d65e3f0c66
Sree Kommalapati <sree.n.kommalapati@gmail.com>
2024-08-30 02:21:33 -0400

    Adding sprite animation frame constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,8 +1,26 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 namespace GameConstants {
+
 	extern const unsigned int SCREEN_WIDTH;
 	extern const unsigned int SCREEN_HEIGHT;
+
 	extern const float WALL_LEFT;
 	extern const float WALL_RIGHT;
+
+	extern const sf::IntRect[] PLAYER_FRAMES;
+	extern const sf::IntRect[] PLAYER_PROJECTILE_FRAMES;
+	
+	extern const sf::IntRect[] ENEMY1_FRAMES;
+	extern const sf::IntRect[] ENEMY1_PROJECTILE_FRAMES;
+
+	extern const sf::IntRect[] ENEMY2_FRAMES;
+	extern const sf::IntRect[] ENEMY2_PROJECTILE_FRAMES;
+
+	extern const sf::IntRect[] ENEMY3_FRAMES;
+	extern const sf::IntRect[] ENEMY3_PROJECTILE_FRAMES;
+
+	extern const sf::IntRect[] ENEMY4_FRAMES;
+	extern const sf::IntRect[] ENEMY4_PROJECTILE_FRAMES;
 }
--- src/constants.cpp
+++ src/constants.cpp
@@ -1,8 +1,21 @@
 #include "constants.hpp"
 
 namespace GameConstants {
-	const unsigned int SCREEN_WIDTH = 1024u;
-	const unsigned int SCREEN_HEIGHT = 896u;
-	const float WALL_LEFT = 5.0f;
-	const float WALL_RIGHT = 1019.0f;
+	constexpr unsigned int SCREEN_WIDTH = 1024u;
+	constexpr unsigned int SCREEN_HEIGHT = 896u;
+	constexpr float WALL_LEFT = 5.0f;
+	constexpr float WALL_RIGHT = 1019.0f;
+	
+	constexpr sf::IntRect[] PLAYER_FRAMES = { sf::IntRect(1, 49, 16, 8) };
+	constexpr sf::IntRect[] PLAYER_PROJECTILE_FRAMES = { sf::IntRect(55, 49, 1, 8) };
+	
+	constexpr sf::IntRect[] ENEMY1_FRAMES = { sf::IntRect(1, 1, 16, 8), sf::IntRect(1, 11, 16, 8) };
+	constexpr sf::IntRect[] ENEMY1_PROJECTILE_FRAMES = { sf::IntRect(1, 21, 3, 8), sf::IntRect(6, 21, 3, 8), sf::IntRect(11, 21, 3, 8), sf::IntRect(16, 21, 3, 8) };
+										   
+	constexpr sf::IntRect[] ENEMY2_FRAMES = { sf::IntRect(19, 1, 16, 8), sf::IntRect(19, 11, 16, 8) };
+	constexpr sf::IntRect[] ENEMY2_PROJECTILE_FRAMES = { sf::IntRect(21, 21, 3, 8), sf::IntRect(26, 21, 3, 8), sf::IntRect(31, 21, 3, 8), sf::IntRect(36, 21, 3, 8) };
+
+	constexpr sf::IntRect[] ENEMY3_FRAMES = { sf::IntRect(37, 1, 16, 8), sf::IntRect(37, 11, 16, 8) };
+	constexpr sf::IntRect[] ENEMY3_PROJECTILE_FRAMES = { sf::IntRect(41, 21, 3, 8), sf::IntRect(46, 21, 3, 8), sf::IntRect(51, 21, 3, 8), sf::IntRect(56, 21, 3, 8), };
+										   
 }
