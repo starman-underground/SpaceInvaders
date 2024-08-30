730733b4123b958a48765b0cfd9f5ddd44cb6ab0
Sree Kommalapati <sree.n.kommalapati@gmail.com>
2024-08-30 02:22:09 -0400

    Refactoring input to seperate method

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,25 @@
 #include "player.hpp"
 
 
+void handleInput(Player& player, const sf::Event& event) {
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.scancode == sf::Keyboard::Scan::Left || event.key.scancode == sf::Keyboard::Scan::A) {
+			player.movingLeft = true;
+		}
+		if (event.key.scancode == sf::Keyboard::Scan::Right || event.key.scancode == sf::Keyboard::Scan::D) {
+			player.movingRight = true;
+		}
+	} else if (event.type == sf::Event::KeyReleased) {
+		if (event.key.scancode == sf::Keyboard::Scan::Left || event.key.scancode == sf::Keyboard::Scan::A) {
+			player.movingLeft = false;
+		}
+		if (event.key.scancode == sf::Keyboard::Scan::Right || event.key.scancode == sf::Keyboard::Scan::D) {
+			player.movingRight = false;
+		}
+	}
+
+}
+
 int main() {
 	auto window = sf::RenderWindow{ sf::VideoMode(GameConstants::SCREEN_WIDTH, GameConstants::SCREEN_HEIGHT), "Space Invaders" };
 	window.setFramerateLimit(120);
@@ -16,27 +35,18 @@ int main() {
 	Player player = Player(spriteSheet);
 
 	while (window.isOpen()) {
+		
+
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
-			} else if (event.type == sf::Event::KeyPressed) {
-				if (event.key.scancode == sf::Keyboard::Scan::Left || event.key.scancode == sf::Keyboard::Scan::A) {
-					player.movingLeft = true;
-				}
-				if (event.key.scancode == sf::Keyboard::Scan::Right || event.key.scancode == sf::Keyboard::Scan::D) {
-					player.movingRight = true;
-				}
-			} else if (event.type == sf::Event::KeyReleased) {
-				if (event.key.scancode == sf::Keyboard::Scan::Left || event.key.scancode == sf::Keyboard::Scan::A) {
-					player.movingLeft = false;
-				}
-				if (event.key.scancode == sf::Keyboard::Scan::Right || event.key.scancode == sf::Keyboard::Scan::D) {
-					player.movingRight = false;
-				}
+			} else if (event.type == sf::Event::KeyPressed || event.type == sf::Event::KeyReleased) {
+				handleInput(player, event);
 			}
 
 		}
+		
 		player.move();
 
 		window.clear(sf::Color::Black);
